124a90db863680d7386bea7e5882611524f9b45d
Allwin <allwinselva7@gmail.com>
2026-07-21 13:51:19 +0530

    Removed server file

--- DriftFullStack/Server/src/app.ts
+++ DriftFullStack/Server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import http from "http";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 6969;
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
