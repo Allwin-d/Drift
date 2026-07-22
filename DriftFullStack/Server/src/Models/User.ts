7aecbb6a050f5fae6bb1be5ee46cc070cc650603
Allwin <allwinselva7@gmail.com>
2026-07-22 23:55:10 +0530

    Implemented User Schema

---
+++ DriftFullStack/Server/src/Models/User.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+    maxLength: [100, "Name Should be within 100 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    unique: true,
+    maxLength: [254, "Email should be within 254 characters"],
+  },
+  passwordHash: {
+    type: String,
+    required: [true, "Password is required"],
+    maxLength: [255, "Password should be within 255 characters"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
