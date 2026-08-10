21dcd5c161c8a7cd2862536a7a4f210c740e7716
Allwin <allwinselva7@gmail.com>
2026-08-10 14:31:15 +0530

    Get entries functionality completed successfully

--- DriftFullStack/Server/src/Controllers/entryControllers/entryController.ts
+++ DriftFullStack/Server/src/Controllers/entryControllers/entryController.ts
@@ -9,11 +9,17 @@ export type userType = {
   createdAt: string;
 };
 
+export type filterObjType = {
+  userId: Types.ObjectId;
+  mood?: number;
+};
+
 export const createEntry = async (req: Request, res: Response) => {
   try {
     console.log("Request.user info :", req.user);
     const { _id } = req.user as userType;
     const { content, mood, lat, lng } = req.body;
+    console.log("Content details : ", content, mood, lat, lng);
 
     const userEntry = await Entry.create({
       userId: _id,
@@ -44,4 +50,39 @@ export const createEntry = async (req: Request, res: Response) => {
   }
 };
 
-export default { createEntry };
+export const getEntries = async (req: Request, res: Response) => {
+  try {
+    const { page, limit, mood } = req.query;
+    const user = req.user as userType;
+
+    const filterObj: filterObjType = {
+      userId: user._id,
+    };
+
+    if (mood) {
+      filterObj.mood = Number(mood);
+    }
+
+    const pageNum = page ? Number(page) : 1;
+    const limitNum = limit ? Number(limit) : 10;
+    const skip = (pageNum - 1) * limitNum;
+
+    const userEntries = await Entry.find(filterObj)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limitNum);
+
+    return res.status(200).json({
+      success: true,
+      message: "User Entries fetched successfully",
+      data: userEntries,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+
+export default { createEntry, getEntries };
--- DriftFullStack/Server/src/Routes/entryRoutes/entryRoutes.ts
+++ DriftFullStack/Server/src/Routes/entryRoutes/entryRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import verifyToken from "../../Middleware/auth.middleware.js";
-import { createEntry } from "../../Controllers/entryControllers/entryController.js";
+import {
+  createEntry,
+  getEntries,
+} from "../../Controllers/entryControllers/entryController.js";
 
 const router = express.Router();
 
+router.get("/", verifyToken, getEntries);
 router.post("/", verifyToken, createEntry);
 
 export default router;
