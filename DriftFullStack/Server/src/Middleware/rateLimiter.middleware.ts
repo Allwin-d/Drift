9a61f82186ac1c61db38b3df384323bbd4129c5f
Allwin <allwinselva7@gmail.com>
2026-09-17 12:19:46 +0530

    Implemented rate Limiter functionality in order to avoid the brute force attacks

--- DriftFullStack/Server/src/Middleware/rateLimiter.middleware.ts
+++ DriftFullStack/Server/src/Middleware/rateLimiter.middleware.ts
@@ -1,15 +1,14 @@
 import rateLimit from "express-rate-limit";
 
-
 const rateLimiter = rateLimit({
-    limit : 5 ,
-    windowMs : 15 * 60 * 1000,
-    message : {
-        success : false,
-        message : "Too many attempts . Please try again later"
-    },
-    standardHeaders : "draft-7",
-    legacyHeaders : false
-})
+  limit: 5,
+  windowMs: 15 * 60 * 1000,
+  message: {
+    success: false,
+    message: "Too many attempts . Please try again later",
+  },
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
 
 export default rateLimiter;
